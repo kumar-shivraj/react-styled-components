039e4781b1a035258b93d8531f4b39c619779fe6
SHIVRKUM <SHIVRKUM@SHIVRKUM-T490.us.oracle.com>
2024-01-26 09:29:09 +0530

    using props in styled components, extending styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
-import StyledButton from "./components/Button/Button";
+import StyledButton, { FancyButton } from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       {/* <button>Button</button> */}
       <StyledButton>StyledButton</StyledButton>
+      <div>
+        <br />
+      </div>
+      <StyledButton variant="outline">StyledButton</StyledButton>
+      <div>
+        <br />
+      </div>
+      <FancyButton>Fancy Button</FancyButton>
+
+      <div>
+        <br />
+      </div>
+      <FancyButton as="a">Fancy Button with polymorphic tag</FancyButton>
     </div>
   );
 }
